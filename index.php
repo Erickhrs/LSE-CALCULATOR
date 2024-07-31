d48712b88a1087de035173422f2395dec1a6ab29
Erickhrs <erickhenriquee4444@gmail.com>
2024-07-31 17:16:06 -0300

    UPDATING LAYOUT

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="./styles/global.css">
+    <title>Calculadora</title>
+</head>
+
+<body>
+    <nav>
+        <img src="https://lsetiquetas.ind.br/wp-content/uploads/2024/05/logo-novo.png" alt="logo">
+    </nav>
+    <main id="root">
+
+    </main>
+</body>
+
+</html>
