336f0eb35ef317d8ab4ea78aea5b3f921a095d74
Erickhrs <erickhenriquee4444@gmail.com>
2024-09-20 11:51:14 -0300

    changing layout and creating select

--- index.php
+++ index.php
@@ -11,9 +11,31 @@
 
 <body>
     <nav>
-        <img src="https://lsetiquetas.ind.br/wp-content/uploads/2024/05/logo-novo.png" alt="logo">
+        <img src="./assets/logo.png" alt="logo">
     </nav>
     <main id="root">
+        <div id="img">
+            <div id="product_info">
+                <h3 id="title">Etiqueta Bordada</h3>
+                <span>As etiquetas bordadas da LS Etiquetas são confeccionadas para aplicação no vestuário, como roupas,
+                    roupas íntimas e moda praia, cama, mesa e banho. Também são usadas em tapetes, bolsas, malas, enfim,
+                    na confecção em geral.
+
+                    Indispensáveis para a identidade do produto nossas etiquetas bordadas podem ser produzidas em
+                    diferentes tipos de fio, com até oito cores, variados padrões de dobra e corte, em alta definição,
+                    tafetá, metalizadas, termocolantes e cortadas a laser.
+
+                    Além de etiquetas personalizadas com marcas, logotipos, coleções e outros, a LS Etiquetas
+                    confecciona etiquetas com instruções de lavagem, composição de material, tamanhos e outros padrões
+                    técnicos exigidos pelo mercado.</span>
+            </div>
+        </div>
+
+        <form action="">
+            <select name="product" id="product">
+
+            </select>
+        </form>
 
     </main>
 </body>
