f72be38d54ab74f49bff7d89dca0978a280bad30
Erickhrs <erickhenriquee4444@gmail.com>
2024-09-23 15:34:11 -0300

    display calculator

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                 </select>
             </div>
 
-            <div id="forms">
+            <forms class="forms" id="eb">
                 <div>
                     <label for="width">Largura</label>
                     <select name="width" id="width_in">
@@ -158,9 +158,226 @@
                     <label for="units">Unidades</label>
                     <input type="number" name="units">
                 </div>
-            </div>
+            </forms>
+            <forms class="forms" id="fg">
+                <div>
+                    <label for="width">Largura</label>
+                    <select name="width" id="width_in">
+                        <option value="12">12mm</option>
+                        <option value="15">15mm</option>
+                        <option value="18">18mm</option>
+                        <option value="22">22mm</option>
+                        <option value="25">25mm</option>
+                        <option value="30">30mm</option>
+                        <option value="33">33mm</option>
+                        <option value="36">36mm</option>
+                        <option value="40">40mm</option>
+                        <option value="45">45mm</option>
+                        <option value="50">50mm</option>
+                        <option value="60">60mm</option>
+                        <option value="61">61mm</option>
+                        <option value="62">62mm</option>
+                        <option value="63">63mm</option>
+                        <option value="64">64mm</option>
+                        <option value="65">65mm</option>
+                        <option value="66">66mm</option>
+                        <option value="67">67mm</option>
+                        <option value="68">68mm</option>
+                        <option value="69">69mm</option>
+                        <option value="70">70mm</option>
+                        <option value="71">71mm</option>
+                        <option value="72">72mm</option>
+                        <option value="73">73mm</option>
+                        <option value="74">74mm</option>
+                        <option value="75">75mm</option>
+                        <option value="76">76mm</option>
+                        <option value="77">77mm</option>
+                        <option value="78">78mm</option>
+                        <option value="79">79mm</option>
+                        <option value="80">80mm</option>
+                        <option value="81">81mm</option>
+                        <option value="82">82mm</option>
+                        <option value="83">83mm</option>
+                        <option value="84">84mm</option>
+                        <option value="85">85mm</option>
+                        <option value="86">86mm</option>
+                        <option value="87">87mm</option>
+                        <option value="88">88mm</option>
+                        <option value="89">89mm</option>
+                        <option value="90">90mm</option>
+                        <option value="91">91mm</option>
+                        <option value="92">92mm</option>
+                        <option value="93">93mm</option>
+                        <option value="94">94mm</option>
+                        <option value="95">95mm</option>
+                        <option value="96">96mm</option>
+                        <option value="97">97mm</option>
+                        <option value="98">98mm</option>
+                        <option value="99">99mm</option>
+                        <option value="100">100mm</option>
+
+                    </select>
+                </div>
+                <div>
+                    <label for="color">Cores</label>
+                    <select name="colors" id="colors_in">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="wire">Tipo de Fio</label>
+                    <select name="wire" id="wire_in">
+                        <option value="metalizado">metalizado</option>
+                        <option value="poliester">Poliester</option>
+                        <option value="trilobal">trilobal</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="quality">Qualidade</label>
+                    <select name="quality" id="quality_in">
+                        <option value="tafeta">Tefetá</option>
+                        <option value="mad">Meia Alta Definição</option>
+                        <option value="ad">Alta Definição</option>
+                        <option value="ad2">AD2</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="units">Unidades</label>
+                    <input type="number" name="units">
+                </div>
+            </forms>
+            <forms class="forms" id="pe">
+                <div>
+                    <label for="width">Largura</label>
+                    <select name="width" id="width_in">
+                        <option value="12">12mm</option>
+                        <option value="15">15mm</option>
+                        <option value="18">18mm</option>
+                        <option value="22">22mm</option>
+                        <option value="25">25mm</option>
+                        <option value="30">30mm</option>
+                        <option value="33">33mm</option>
+                        <option value="36">36mm</option>
+                        <option value="40">40mm</option>
+                        <option value="45">45mm</option>
+                        <option value="50">50mm</option>
+                        <option value="60">60mm</option>
+                        <option value="61">61mm</option>
+                        <option value="62">62mm</option>
+                        <option value="63">63mm</option>
+                        <option value="64">64mm</option>
+                        <option value="65">65mm</option>
+                        <option value="66">66mm</option>
+                        <option value="67">67mm</option>
+                        <option value="68">68mm</option>
+                        <option value="69">69mm</option>
+                        <option value="70">70mm</option>
+                        <option value="71">71mm</option>
+                        <option value="72">72mm</option>
+                        <option value="73">73mm</option>
+                        <option value="74">74mm</option>
+                        <option value="75">75mm</option>
+                        <option value="76">76mm</option>
+                        <option value="77">77mm</option>
+                        <option value="78">78mm</option>
+                        <option value="79">79mm</option>
+                        <option value="80">80mm</option>
+                        <option value="81">81mm</option>
+                        <option value="82">82mm</option>
+                        <option value="83">83mm</option>
+                        <option value="84">84mm</option>
+                        <option value="85">85mm</option>
+                        <option value="86">86mm</option>
+                        <option value="87">87mm</option>
+                        <option value="88">88mm</option>
+                        <option value="89">89mm</option>
+                        <option value="90">90mm</option>
+                        <option value="91">91mm</option>
+                        <option value="92">92mm</option>
+                        <option value="93">93mm</option>
+                        <option value="94">94mm</option>
+                        <option value="95">95mm</option>
+                        <option value="96">96mm</option>
+                        <option value="97">97mm</option>
+                        <option value="98">98mm</option>
+                        <option value="99">99mm</option>
+                        <option value="100">100mm</option>
+
+                    </select>
+                </div>
+                <div>
+                    <label for="height">Comprimento(mm)</label>
+                    <input type="number" name="height">
+                </div>
+                <div>
+                    <label for="color">Cores</label>
+                    <select name="colors" id="colors_in">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="wire">Tipo de Fio</label>
+                    <select name="wire" id="wire_in">
+                        <option value="metalizado">metalizado</option>
+                        <option value="poliester">Poliester</option>
+                        <option value="trilobal">trilobal</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="quality">Qualidade</label>
+                    <select name="quality" id="quality_in">
+                        <option value="tafeta">Tefetá</option>
+                        <option value="mad">Meia Alta Definição</option>
+                        <option value="ad">Alta Definição</option>
+                        <option value="ad2">AD2</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="ironed">Engomada</label>
+                    <select name="ironedd" id="ironed_in">
+                        <option value="no">Não</option>
+                        <option value="yes">Sim</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="iron-on">Termocolante</label>
+                    <select name="iron-on" id="iron-on_in">
+                        <option value="no">Não</option>
+                        <option value="yes">Sim</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="units">Unidades</label>
+                    <input type="number" name="units">
+                </div>
+            </forms>
         </div>
     </main>
+    <div id="result_container">
+
+    </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="./scripts/products.js"></script>
--- scripts/products.js
+++ scripts/products.js
@@ -13,6 +13,19 @@ document.addEventListener("DOMContentLoaded", function() {
                         $('#title').html(response.product['product']); 
                         $('#bio').html(response.product['bio']); 
                         $('#img').css('background-image', 'url(' + response.product['img'] + ')');
+
+                        document.getElementById('eb').style.display = 'none'; 
+                        document.getElementById('fg').style.display = 'none'; 
+                        document.getElementById('pe').style.display = 'none'; 
+
+                        if (response.product['product'] == 'Etiqueta Bordada'){
+                            document.getElementById('eb').style.display = 'grid'; 
+                        } else if (response.product['product'] == 'Fita/Galão'){
+                            document.getElementById('fg').style.display = 'grid'; 
+                        } else if (response.product['product'] == 'Patch/Emblema') {
+                            document.getElementById('pe').style.display = 'grid'; 
+                        }
+
                     },
                     error: function(xhr, status, error) {
                         console.error('Erro: ' + error);
